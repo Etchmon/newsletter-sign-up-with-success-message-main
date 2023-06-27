fcc5e003666f577b2b67fccc892becb0a9f7d4a8
James Etchells <etch.rush@gmail.com>
2023-06-26 20:56:36 -0700

    modified:   app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', function () {
-    var img = document.getElementById('img');
-    if (this.window.matchMedia('(min-width:1440px').matches) {
-        img.src = "./assets/images/illustration-sign-up-desktop.svg"
-    }
-    else {
-        img.src = "./assets/images/illustration-sign-up-mobile.svg"
-    }
-});
