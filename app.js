a19131f730af9bd50234a7fd2caf87075dc0c808
James Etchells <etch.rush@gmail.com>
2023-06-27 16:22:18 -0700

    ADD success modal that slides in on submit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const Validator = (() => {
     const form = document.getElementById('form');
+    const success = document.getElementById('success');
+    const card = document.getElementById('card');
 
     const isEmailValid = (email) => {
         // Email pattern for basic validation
@@ -14,8 +16,6 @@ const Validator = (() => {
         const emailValue = form.elements.email.value.trim();
         const errorMsg = document.getElementById('error');
 
-        console.log(errorMsg);
-
         // Perform validations
         if (!isEmailValid(emailValue)) {
             console.log('Failed to submit')
@@ -26,9 +26,19 @@ const Validator = (() => {
 
         email.classList.remove('invalid');
         errorMsg.style.display = 'none';
+        success.style.top = '0';
+        card.style.display = 'none';
+
         console.log('Success!');
 
 
         form.reset();
-    })
+    });
+
+    success.addEventListener('click', function (e) {
+        e.preventDefault();
+        card.style.display = 'grid';
+
+        success.style.top = '-200%';
+    });
 })();
